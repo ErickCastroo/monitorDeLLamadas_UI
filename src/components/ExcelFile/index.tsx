5f4beec55f6a4c3527eac0f9ed9306399e50353b
ErickCastroo <castrocamachoerickmiguel21@gmail.com>
2025-06-24 09:41:18 -0700

    Agregar componente ExcelFile, incluir react-icons y actualizar estilos en Layout

---
+++ src/components/ExcelFile/index.tsx
@@ -0,0 +1,50 @@
+import axios from 'axios'
+import { useState } from 'react'
+
+function ExcelFile() {
+  const [file, setFile] = useState<File | null>(null)
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setFile(e.target.files[0])
+    }
+  }
+
+  const peticion = async (e: React.FormEvent) => {
+    e.preventDefault()
+    if (!file) return alert('Selecciona un archivo')
+
+    const formData = new FormData()
+    formData.append('file', file)
+
+    try {
+      const response = await axios.post('http://localhost:3000/api/clientes/upload', formData, {
+        headers: {
+          'Content-Type': 'multipart/form-data'
+        }
+      })
+      console.log('Respuesta del servidor:', response.data)
+    } catch (error) {
+      console.error('Error al subir el archivo:', error)
+    }
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <form onSubmit={peticion}>
+        <input
+          type="file"
+          name="file"
+          id="file"
+          onChange={handleChange}
+          className="file-input file-input-bordered file-input-primary w-full max-w-xs"
+        />
+        <button type="submit" className="btn btn-primary mt-4">
+          Enviar
+        </button>
+      </form>
+    </div>
+  )
+}
+
+export { ExcelFile }
--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,15 +1,45 @@
-import { Outlet } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
+
+import { PiPhoneCallBold, PiMicrosoftExcelLogoBold } from 'react-icons/pi'
+import { TbSettingsBolt } from 'react-icons/tb'
+
+
 
 function Layout() {
   return (
     <>
-      <header className='bg-indigo-800 p-1'>
+      <header className='bg-Secondary text-white py-2'>
         <div className='max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center'>
+          <div className='w-64'>
+            <Link to='/' className='flex items-center'>
+              <img src='./logooomapasBlanco.png' alt='Logo' className='h-16' />
+            </Link>
+          </div>
+          <div>
+            <nav className='flex space-x-10'>
+              <Link to='/' className='flex flex-col items-center justify-center text-2xl hover:text-Primary dark:hover:text-PrimaryDark'>
+                <PiPhoneCallBold className='text-3xl' />
+                <span className='text-xs'>Llamadas</span>
+              </Link>
+
+              <Link to='/' className='flex flex-col items-center justify-center text-2xl hover:text-Primary dark:hover:text-PrimaryDark'>
+                <PiMicrosoftExcelLogoBold className='text-3xl' />
+                <span className='text-xs'>Excel</span>
+              </Link>
+
+              <Link to='/' className='flex flex-col items-center justify-center text-2xl hover:text-Primary dark:hover:text-PrimaryDark'>
+                <TbSettingsBolt className='text-3xl' />
+                <span className='text-xs'>Configuración</span>
+              </Link>
+            </nav>
+          </div>
         </div>
       </header>
-      <section className='max-w-screen-2xl mx-auto mt-10'>
-        <Outlet />
-      </section>
+      <body className='bg-Background text-TextColor'>
+        <section className='max-w-screen-2xl mx-auto mt-10'>
+          <Outlet />
+        </section>
+      </body>
     </>
   )
 }
